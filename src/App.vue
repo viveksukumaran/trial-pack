c2f32196b372fc62af7b94dfbeb3113f068cf9e1
Vivek Sukumaran <vivek.sukumaran@yahoo.co.in>
2020-07-18 17:30:38 +0530

    initial working version

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!--<img alt="Vue logo" src="./assets/logo.png">    
+    <HelloWorld msg="Welcome Vivek to Your Vue.js App"/>-->
+    <trial-pack/>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue';
+import TrialPack from './components/TrialPack.vue';
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld,
+    TrialPack
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TrialPack.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="trial-pack">
+    <div>Choose size</div>
+    <div>
+    <label v-for="detail in images" v-bind:key="detail.size">
+      <input
+        type="radio"
+        name="size"
+        v-model="selectedSize"
+        v-bind:value="detail.size"
+        v-on:click="changeImg(detail)"
+      />
+      {{detail.size}}
+    </label></div>
+    <img :src="selectedImage" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TrialPack",
+  data() {
+    return {
+      images: [
+        { size: "1(2-5 KG)", image: "/assets/lillydoo-testpaket-10.jpg" },
+        { size: "2(4-8 KG)", image: "/assets/lillydoo-testpaket-20.jpg" },
+        { size: "3(6-10 KG)", image: "/assets/lillydoo-testpaket-30.jpg" },
+        { size: "4(9-14 KG)", image: "/assets/lillydoo-testpaket-40.jpg" },
+        { size: "5(11-16 KG)", image: "/assets/lillydoo-testpaket-50.jpg" }
+      ],
+      selectedImage: "/assets/lillydoo-testpaket-10.jpg",
+      selectedSize: "1(2-5 KG)"      
+    };
+  },
+  methods: {
+    changeImg(source) {
+      this.selectedImage = source.image;
+      this.selectedSize = source.size;
+    }
+  }
+};
+// this.selected = this.images[0];
+</script>
