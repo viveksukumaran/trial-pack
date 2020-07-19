2e59535363c59ad68a4df4febf3ab85bc2730320
Vivek Sukumaran <vivek.sukumaran@yahoo.co.in>
2020-07-19 13:08:15 +0530

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="app">
-    <!--<img alt="Vue logo" src="./assets/logo.png">    
-    <HelloWorld msg="Welcome Vivek to Your Vue.js App"/>-->
     <trial-pack/>
   </div>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue';
 import TrialPack from './components/TrialPack.vue';
 
 export default {
   name: 'App',
   components: {
-    //HelloWorld,
     TrialPack
   }
 }
